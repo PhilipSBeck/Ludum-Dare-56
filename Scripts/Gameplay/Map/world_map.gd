5469a557c31ba60ad54cb582388fc996007d9bec
philipbeck <psbeck+1@gmail.com>
2024-10-05 10:43:37 +0100

    TileMap added

--- Scripts/Gameplay/Map/world_map.gd
+++ Scripts/Gameplay/Map/world_map.gd
@@ -5,7 +5,7 @@ extends TileMapLayer
 func _ready() -> void:
 	for i in 100:
 		for j in 100:
-			set_cell(Vector2i(i, j), 0)
+			set_cell(Vector2i(i, j), 0, Vector2i(0,0))
 			
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
