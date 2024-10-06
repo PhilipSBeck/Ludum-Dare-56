079d02d31452ec041eb18b6031df7d3b19e96190
philipbeck <psbeck+1@gmail.com>
2024-10-06 16:39:45 +0100

    Added Second enemy skin

--- Scripts/Units/Enemies/animated_sprite.gd
+++ Scripts/Units/Enemies/animated_sprite.gd
@@ -3,6 +3,7 @@ extends AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	set_animation("Enemy" + str(randi_range(1,2)))
 	$".".play()
 	pass # Replace with function body.
 
